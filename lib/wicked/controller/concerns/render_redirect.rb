3b75b5c43e22737952768c376870ca23d00f17eb
Vincent Filby <vfilby@gmail.com>
2012-07-18 16:35:21 -0400

    Adding redirect parameters

--- lib/wicked/controller/concerns/render_redirect.rb
+++ lib/wicked/controller/concerns/render_redirect.rb
@@ -2,11 +2,11 @@ module Wicked::Controller::Concerns::RenderRedirect
   extend ActiveSupport::Concern
 
 
-  def render_wizard(resource = nil)
+  def render_wizard(resource = nil, parameters = {})
     process_resource!(resource)
 
     if @skip_to
-      redirect_to wizard_path @skip_to
+      redirect_to wizard_path @skip_to, parameters
     else
       render_step @step
     end
@@ -30,11 +30,11 @@ module Wicked::Controller::Concerns::RenderRedirect
     end
   end
 
-  def redirect_to_next(next_step)
+  def redirect_to_next(next_step, parameters = {})
     if next_step.nil?
       redirect_to_finish_wizard
     else
-      redirect_to wizard_path(next_step)
+      redirect_to wizard_path next_step, parameters
     end
   end
 
